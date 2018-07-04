a19bb66e55a31ab42316888df6c0a4eeb353e692
eroberer <efatih0@gmail.com>
2018-07-05 00:48:07 +0300

    logout

--- src/components/Header/HeaderDropdown.vue
+++ src/components/Header/HeaderDropdown.vue
@@ -2,21 +2,32 @@
       <b-nav-item-dropdown right no-caret>
         <template slot="button-content">
           {{ getUserName() }}
-          <img src="static/img/avatars/6.jpg" class="img-avatar">
+          <img :src="image" class="img-avatar" width="40" height="40" style="border:1px solid #efefef">
         </template>
         <b-dropdown-header tag="div" class="text-center"><strong>Account</strong></b-dropdown-header>
-        <b-dropdown-item><i class="fa fa-user"></i> Profile</b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-wrench"></i> Settings</b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-lock"></i> Logout</b-dropdown-item>
+        <b-dropdown-item><i class="fa fa-user"></i> Profil</b-dropdown-item>
+        <b-dropdown-item @click="logout"><i class="fa fa-lock"></i> Çıkış</b-dropdown-item>
       </b-nav-item-dropdown>
 </template>
 <script>
 export default {
   name: "header-dropdown",
-  data: () => {
-    return { itemsCount: 42 };
+  data() {
+    return {
+      image: ""
+    };
   },
+
+  created() {
+    this.image = JSON.parse(sessionStorage.getItem("info")).resim;
+  },
+
   methods: {
+    logout() {
+      sessionStorage.clear();
+      location.reload();
+    },
+
     getUserName() {
       var user = JSON.parse(sessionStorage.getItem("info"));
       return user.ad_soyad;
